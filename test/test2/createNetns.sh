3b10130578ce93839710181361914fce51b84028
YYR <315629555@qq.com>
2019-12-10 10:33:52 +0800

    [modify] 更改test2测试文件，使得自行实现的路由器所在netns不配置ip地址

--- test/test2/createNetns.sh
+++ test/test2/createNetns.sh
@@ -15,14 +15,14 @@ ip link set veth-2r netns net2
 ip link set veth-3l netns net3
 ip link set veth-3r netns net3
 ip link set veth-4l netns net4
-ip netns exec net0 ip addr add 192.168.2.1/24 dev veth-0r
-ip netns exec net1 ip addr add 192.168.2.2/24 dev veth-1l
+ip netns exec net0 ip addr add 192.168.1.2/24 dev veth-0r
+ip netns exec net1 ip addr add 192.168.1.1/24 dev veth-1l
 ip netns exec net1 ip addr add 192.168.3.1/24 dev veth-1r
-ip netns exec net2 ip addr add 192.168.3.2/24 dev veth-2l
-ip netns exec net2 ip addr add 192.168.4.1/24 dev veth-2r
+# ip netns exec net2 ip addr add 192.168.3.2/24 dev veth-2l
+# ip netns exec net2 ip addr add 192.168.4.1/24 dev veth-2r  # 不配置IP ?
 ip netns exec net3 ip addr add 192.168.4.2/24 dev veth-3l
-ip netns exec net3 ip addr add 192.168.5.1/24 dev veth-3r
-ip netns exec net4 ip addr add 192.168.5.2/24 dev veth-4l
+ip netns exec net3 ip addr add 192.168.5.2/24 dev veth-3r
+ip netns exec net4 ip addr add 192.168.5.1/24 dev veth-4l
 ip netns exec net0 ip link set veth-0r up
 ip netns exec net1 ip link set veth-1l up
 ip netns exec net1 ip link set veth-1r up
@@ -31,6 +31,8 @@ ip netns exec net2 ip link set veth-2r up
 ip netns exec net3 ip link set veth-3l up
 ip netns exec net3 ip link set veth-3r up
 ip netns exec net4 ip link set veth-4l up
+ip netns exec net0 ip r add default via 192.168.1.1
+ip netns exec net4 ip r add default via 192.168.5.2  # 为两台PC配置网关
 ip netns exec net0 ip link set lo up
 ip netns exec net1 ip link set lo up
 ip netns exec net2 ip link set lo up
